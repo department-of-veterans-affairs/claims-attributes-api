fb0ff8428fc5aff61a8a591b56392b3aa62c7ef6
Nat Hillard <nathaniel.hillard@va.gov>
2020-12-13 11:49:17 -0500

    updating flashes service

--- flashes_service/main.py
+++ flashes_service/main.py
@@ -1,51 +1,52 @@
-from caapi_shared.utils import clean_text, find_similar
+from caapi_shared.utils as utils
 from caapi_shared.schemas import Flash
 from fastapi import FastAPI
 
 app = FastAPI()
 
-def find_flashes(x):
-    flashes = []
-    if "hardship" in x:
-        flashes.append("Hardship")
-    if "seriously injured" in x:
-        flashes.append("Seriously Injured/Very Seriously Injured")
-    if "terminally ill" in x:
-        flashes.append("Terminally Ill")
-    if "homeless" in x:
-        flashes.append("Homeless")
-    if "purple heart" in x:
-        flashes.append("Purple Heart")
-    if ("pow" in x) or ("prisoner of war" in x) or ("p o w" in x):
-        flashes.append("POW")
-    if "medal of honor" in x:
-        flashes.append("Medal of Honor")
-    if ("amyotrophic lateral sclerosis" in x) or ("als" in x) or ("a l s" in x):
-        flashes.append("Amyotrophic Lateral Sclerosis")
-    if "emergency care" in x:
-        flashes.append("Emergency Care")
-    return flashes
+class SpecialIssuesClassifier:
+    def __init__(self):
+        self.flashes_map = json.load(Path("flashes.json").read_text())
 
+    def check_keywords(self, claim_text):
+        flashes = []
+        if "hardship" in claim_text:
+            flashes.append("Hardship")
+        if "seriously injured" in claim_text:
+            flashes.append("Seriously Injured/Very Seriously Injured")
+        if "terminally ill" in claim_text:
+            flashes.append("Terminally Ill")
+        if "homeless" in claim_text:
+            flashes.append("Homeless")
+        if "purple heart" in claim_text:
+            flashes.append("Purple Heart")
+        if ("pow" in x) or ("prisoner of war" in claim_text) or ("p o w" in claim_text):
+            flashes.append("POW")
+        if "medal of honor" in claim_text:
+            flashes.append("Medal of Honor")
+        if ("amyotrophic lateral sclerosis" in claim_text) or ("als" in x) or ("a l s" in x):
+            flashes.append("Amyotrophic Lateral Sclerosis")
+        if "emergency care" in claim_text:
+            flashes.append("Emergency Care")
+        return flashes
 
-flashesD = {
-    "hardship": "Hardship",
-    "seriously injured": "Seriously Injured/Very Seriously Injured",
-    "terminally ill": "Terminally Ill",
-    "homeless": "Homeless",
-    "purple heart": "Purple Heart",
-    "pow": "POW",
-    "prisoner of war": "POW",
-    "p o w": "POW",
-    "medal of honor": "Medal of Honor",
-    "amyotrophic lateral sclerosis": "Amyotrophic Lateral Sclerosis",
-    "als": "Amyotrophic Lateral Sclerosis",
-    "a l s": "Amyotrophic Lateral Sclerosis",
-    "emergency care": "Emergency Care",
-}
-
+    def classify(self, claim_text):
+        cleaned_text = utils.clean_text(text)
+        keyword_matches = self.check_keywords(cleaned_text)
+        spelling_matches = utils.find_similar(cleaned_text, self.flashes_map)
+        matches = keyword_matches + spelling_matches
+        match_results = []
+        for match in matches:
+            match_results.append({
+                "text": match
+            })
+        return match_results
 
 @app.get("/")
-def get_flashes(text) -> [Flash]:
-    flashes = find_flashes(clean_text(text)) + find_similar(clean_text(text), flashesD)
-    return [{"text": flash} for flash in flashes]  # leaving off confidence for now
+def get_flashes(claim_input: ClaimInput) -> [Flash]:
+    classifier = FlashesClassifier()
+    flash_list = []
+    for claim_text in claim_input.claim_text:
+        flash_list.append(classifier.classify(claim_text))
+    return flash_list
 
