449650641a9173b2760ea2ccba6fd8c19059f8ac
Nat Hillard <nathaniel.hillard@va.gov>
2020-12-14 23:29:36 -0500

    adding tests to api

--- src/api_service/app/main.py
+++ src/api_service/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, APIRouter
 from fastapi.openapi.utils import get_openapi
 from .routers import healthcheck, predict
 from pathlib import Path
+from importlib_resources import files
 
 # All API calls have this prefix in order to avoid Load Balancer conflicts
 api_prefix = "benefits-claims-attributes"
@@ -19,7 +20,7 @@ def custom_openapi():
     openapi_schema = get_openapi(
         title="Claims Attributes API",
         version="1.0.0",
-        description=Path("claims_attributes/summary.md").read_text(),
+        description=Path(files("app.data").joinpath("summary.md")).read_text(),
         routes=app.routes,
     )
     app.openapi_schema = openapi_schema
@@ -29,5 +30,5 @@ def custom_openapi():
 global_router = APIRouter()
 global_router.include_router(healthcheck.router)
 global_router.include_router(predict.router)
-global_router.openapi = custom_openapi
+global_router.openapi = custom_openapi()
 app.include_router(global_router, prefix=f"/{api_prefix}/{version}")
--- src/api_service/tests/test_main.py
+++ src/api_service/tests/test_main.py
@@ -1,5 +1,5 @@
 from fastapi.testclient import TestClient
-from claims_attributes.main import app
+from app.main import app
 import pytest
 
 client = TestClient(app)
@@ -17,72 +17,3 @@ def test_healthcheck():
     assert response.status_code == 200
     assert response.json() == "App OK"
 
-
-# We set a bogus confidence to avoid testing model specifics
-# See more here: https://docs.pytest.org/en/latest/parametrize.html#pytest-mark-parametrize
-@pytest.mark.parametrize("bogus_confidence",
-                         [96.0])
-def test_predict(bogus_confidence):
-    response = client.post(
-        "/benefits-claims-attributes/v1/",
-        json={
-            "claim_text": [
-                "Ringing in my ear",
-                "cancer due to agent orange",
-                "p.t.s.d from gulf war",
-                "recurring nightmares",
-                "skin condition because of homelessness",
-            ]
-        },
-    )
-    assert response.status_code == 200
-    assert response.json() == {
-        "contentions": [
-            {
-                "specialIssues": [],
-                "flashes": [],
-                "originalText": "Ringing in my ear",
-                "classification": {
-                    "text": "hearing loss",
-                    "confidence": float(f"{bogus_confidence}"),
-                    "code": "3140",
-                },
-            },
-            {
-                "flashes": [],
-                "specialIssues": [{"text": "AOOV"}],
-                "classification": {
-                    "code": "8935",
-                    "confidence": float(f"{bogus_confidence}"),
-                    "text": "cancer - genitourinary",
-                },
-                "originalText": "cancer due to agent orange",
-            },
-            {
-                "originalText": "p.t.s.d from gulf war",
-                "classification": {
-                    "text": "mental disorders",
-                    "code": "8989",
-                    "confidence": float(f"{bogus_confidence}"),
-                },
-                "flashes": [],
-                "specialIssues": [{"text": "GW"}, {"text": "PTSD/1"}],
-            },
-            {
-                "specialIssues": [],
-                "flashes": [],
-                "originalText": "recurring nightmares",
-                "classification": {
-                    "text": "mental disorders",
-                    "code": "8989",
-                    "confidence": float(f"{bogus_confidence}"),
-                },
-            },
-            {
-                "classification": {"confidence": float(f"{bogus_confidence}"), "code": "9016", "text": "skin"},
-                "originalText": "skin condition because of homelessness",
-                "specialIssues": [],
-                "flashes": [{"text": "Homeless"}],
-            },
-        ]
-    }
