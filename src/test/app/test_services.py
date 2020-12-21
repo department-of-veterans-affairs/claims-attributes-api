536c25d58a9b147e673919376a6e7687c116dc96
Nat Hillard <nathaniel.hillard@va.gov>
2020-12-21 00:33:48 -0500

    decomposing docker-compose further, adding test for api, commented for now

--- src/test/app/test_services.py
+++ src/test/app/test_services.py
@@ -1,11 +1,23 @@
-import requests
-from app.settings import settings
 import docker
+import pytest
+import sys
 
-def setup_module(module):
-    client = docker.from_env()
+@pytest.fixture
+def docker_client():
+    return docker.from_env()
 
-def test_services():
-    
-    print(f"Settings: {settings.classifier_uri}")
-    assert True==True
+def test_flashes(docker_client):
+    print("Running Flashes tests...")
+    result = docker_client.containers.run("flashes:latest", "regression-test")
+    print(f"Result: {result}")
+
+def test_special_issues(docker_client):
+    print("Running Special Issues tests...")
+    result = docker_client.containers.run("special_issues:latest", "regression-test")
+    print(f"Result: {result}")
+
+def test_classifier(docker_client):
+    print("Running Classifier tests...")
+    docker_client.containers.run("classifier:latest", "regression-test")
+    print(f"Result: {result}")
+
