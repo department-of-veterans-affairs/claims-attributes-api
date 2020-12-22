aa191bc72c3a35d68f09c5571492380504584ea4
Nat Hillard <nathaniel.hillard@va.gov>
2020-12-21 22:22:09 -0500

    renaming to testing service

--- src/testing_service/app/test_services.py
+++ src/testing_service/app/test_services.py
@@ -6,18 +6,23 @@ import sys
 def docker_client():
     return docker.from_env()
 
+def test_api(docker_client):
+    print("Running API tests...")
+    result = docker_client.containers.run("api:test", "regression-test")
+    print(f"Result: {result}")
+
 def test_flashes(docker_client):
     print("Running Flashes tests...")
-    result = docker_client.containers.run("flashes:latest", "regression-test")
+    result = docker_client.containers.run("flashes:test", "regression-test")
     print(f"Result: {result}")
 
 def test_special_issues(docker_client):
     print("Running Special Issues tests...")
-    result = docker_client.containers.run("special_issues:latest", "regression-test")
+    result = docker_client.containers.run("specialissues:test", "regression-test")
     print(f"Result: {result}")
 
 def test_classifier(docker_client):
     print("Running Classifier tests...")
-    docker_client.containers.run("classifier:latest", "regression-test")
+    docker_client.containers.run("classifier:test", "regression-test")
     print(f"Result: {result}")
 
