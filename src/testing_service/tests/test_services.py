dbf0134876ccdbbc2411814dca86903b5e19c9fa
Nat Hillard <nathaniel.hillard@va.gov>
2020-12-31 00:11:08 -0500

    Adding certificates

--- src/testing_service/tests/test_services.py
+++ src/testing_service/tests/test_services.py
@@ -5,10 +5,11 @@ from app.settings import settings
 
 @pytest.fixture
 def docker_client():
-    d = docker.from_env()
-    print(f"Images: {d.images.list()}")
-    print(f"Containers: {d.containers.list()}")
-    return d
+    tls_config = docker.tls.TLSConfig(ca_cert='/app/cacert.pem')
+    client = docker.DockerClient(base_url='unix://var/run/docker.sock', tls=tls_config)
+    print(f"Images: {client.images.list()}")
+    print(f"Containers: {client.containers.list()}")
+    return client
 
 def test_api(docker_client):
     print("Running API tests...")
